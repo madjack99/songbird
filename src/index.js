d4a29d43a1d3619b510c91bdee1aa577b3ab5c09
madjack <kavanokami@gmail.com>
2020-02-06 17:18:26 +0400

    feat: create basic structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
