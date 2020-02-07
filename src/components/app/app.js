56fa67d31eed6f1efb4dced07a9aec823ae6ab63
madjack <kavanokami@gmail.com>
2020-02-07 12:39:12 +0400

    feat: add state form cur.Quest and corAnswer

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../header';
 import BirdCategory from '../bird-category';
 import Question from '../question';
 import birdsData from '../../services';
+import { generateRandomAnswer } from '../../utils';
 
 import './app.css';
 
 const App = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState(generateRandomAnswer());
+  const currentQuestionData = birdsData[currentQuestion];
+
+  console.log(correctAnswer);
+
   return (
     <div className="app">
       <Header />
